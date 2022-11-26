83ee38a4a86d0ce6311034940a3257cd07092a5a
kovigegi <116081153+kovigegi@users.noreply.github.com>
2022-11-26 18:15:37 +0100

    Add files via upload

---
+++ code/tile.py
@@ -0,0 +1,16 @@
+import pygame 
+from settings import *
+
+class Tile(pygame.sprite.Sprite):
+	def __init__(self,pos,groups):
+		super().__init__(groups)
+		self.image = pygame.image.load('../graphics/test/wall.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
+		self.hitbox = self.rect.inflate(0,-10)
+
+class Asztal(pygame.sprite.Sprite):
+	def __init__(self,pos,groups):
+		super().__init__(groups)
+		self.image = pygame.image.load('../graphics/asztal.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
+		self.hitbox = self.rect.inflate(0,-10)
